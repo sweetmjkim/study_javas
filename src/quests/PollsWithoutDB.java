64878ac1091b9b799db83bb59fbfd24c175b7aba
sweetmjkim <sweet.mjkim@gmail.com>
2024-03-26 01:05:20 +0000

    2023.03.25

--- src/quests/PollsWithoutDB.java
+++ src/quests/PollsWithoutDB.java
@@ -13,10 +13,33 @@ ex) 1. 인터스텔라 선호도
 */
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class PollsWithoutDB {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello Main !");
+        ArrayList<String> listFilm = new ArrayList<String>();
+        listFilm.add("인터스텔라");
+        listFilm.add("해리포터");
+        listFilm.add("파묘");
+
+        ArrayList<String> listQuestion = new ArrayList<String>();
+        listQuestion.add("1) 좋음");
+        listQuestion.add("2) 중간");
+        listQuestion.add("3) 나쁨");
+
+        ArrayList<String> listAnswer = new ArrayList<String>();
+        // 질문의 답을 거치지않고 바로 출력
+        // listAnswer.add("1");
+        // listAnswer.add("3");
+        // listAnswer.add("1");
+        
+        for(int i=0; i<listFilm.size(); i++){
+            System.out.println(listFilm.get(i));
+            System.out.println(listQuestion);
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("당신의 점수를 입력하세요. : ");
+            listAnswer.add(scanner.nextLine());
+        }
+        System.out.println("당신의 평가는" + listAnswer + "입니다.");
     }
 }
